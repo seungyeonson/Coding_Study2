e0f5763f85f0ce54c74c29aea1c4a22f840a29ef
Seungyeon Song <ream123@naver.com>
2020-07-25 00:45:06 +0900

    200724finish

--- 1289_D3.py
+++ 1289_D3.py
@@ -1,18 +1,17 @@
 T = int(input())
 for tc in range(T):
-    arr = list(input())
-    test = [0]*len(arr)
+    answer=0
+    arr = list(input()) #0011
+    test = ['0']*len(arr)  #0000
     for i in range(len(arr)):
-        ['0'] * i + ['1'] * (len(arr) - i
-    # answer=9999
-    # for i in range(len(arr)+1):
-    #     test.append())
-    #
-    # for i in range(len(test)):
-    #     temp = 1
-    #     for j in range(len(test[i])):
-    #         if test[i][j] != arr[j]:
-    #             temp += 1
-    #     if temp < answer:
-    #         answer = temp
+        # 만약 탐색하면서 같으면 패스
+        # 아니면 나머지 자리 반대로 바꾸기=>test를 + 바꾸면 answer+=1
+        if arr[i] == test[i]:
+            continue
+        elif arr[i] != test[i]:
+            answer+=1
+            for j in range(i,len(arr)):
+                test[j] = arr[i]
+
+
     print("#"+str(tc+1), answer)
