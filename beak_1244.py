0edd7046146f565cc094bb5bf79fe3a0148cb871
Seungyeon Song <ream123@naver.com>
2020-08-03 15:59:21 +0900

    200803finish

--- beak_1244.py
+++ beak_1244.py
@@ -7,15 +7,34 @@ for i in range(N):
     order.append(list(map(int, input().split())))
 for i in range(N):
     if order[i][0] == 1:
-        temp = order[i][1]
+        temp = order[i][1] -1
         j = temp
-        while(j<=len(arr)):
-            if arr[j-1] == 0:
-                arr[j - 1] = 1
+        while(j<len(arr)):
+            if arr[j] == 0:
+                arr[j] = 1
             else:
-                arr[j-1] = 0
-            j += temp
-    elif order[i][1] ==2:
-        pass
+                arr[j] = 0
+            j += temp + 1
+    elif order[i][0] == 2:
+        temp = order[i][1] - 1
+        temp_a = []
+        k = 1
+        if temp == order[i][1] - 1:
+            temp_a.append(temp)
+        while 0<=temp - k and temp + k<T:
+            if arr[temp-k] == arr[temp+k]:
+                temp_a.append(temp-k)
+                temp_a.append(temp+k)
+                k += 1
+            else:
+                break
+        for p in range(len(temp_a)):
+            if arr[temp_a[p]] == 0:
+                arr[temp_a[p]] = 1
+            else:
+                arr[temp_a[p]] = 0
+
 for i in range(len(arr)):
-    print(arr[i],end=" ")
+    print(arr[i], end=" ")
+    if(i%20==19):
+        print()
